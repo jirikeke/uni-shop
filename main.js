bef82bf5f867c9a1325dcd922a6686616470fc3e
李龙海 <11604228+llh666666@user.noreply.gitee.com>
2023-01-02 17:33:11 +0800

    完成了home首页的开发

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import {
+  $http
+} from '@escook/request-miniprogram'
+
+uni.$http = $http
+// 配置请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+// 封装的展示消息提示的方法
+uni.$showMsg = function(title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none'
+  })
+}
+
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+$http.afterRequest = function(options) {
+  uni.hideLoading()
+}
+
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+  ...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+  createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
   const app = createSSRApp(App)
