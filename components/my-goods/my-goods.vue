0b1ec29062d09811361c1c458a7fe6d7e9504216
李龙海 <11604228+llh666666@user.noreply.gitee.com>
2023-01-06 15:15:28 +0800

    完成了购物车的开发

--- components/my-goods/my-goods.vue
+++ components/my-goods/my-goods.vue
@@ -2,6 +2,7 @@
   <view class="goods-item">
     <!-- 商品左侧图片区域 -->
     <view class="goods-item-left">
+      <radio :checked="goods.goods_state" color="#C00000" v-if="showRadio" @click="radioClickHandler"></radio>
       <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
     </view>
     <!-- 商品右侧信息区域 -->
@@ -11,6 +12,8 @@
       <view class="goods-info-box">
         <!-- 商品价格 -->
         <view class="goods-price">￥{{goods.goods_price | tofixed}}</view>
+        <!-- 商品数量 -->
+        <uni-number-box :min="1" :value="goods.goods_count" v-if="showNum" @change="numChangeHandler"></uni-number-box>
       </view>
     </view>
   </view>
@@ -26,11 +29,37 @@
         type: Object,
         defaul: {},
       },
+      showRadio: {
+        type: Boolean,
+        default: false,
+      },
+      showNum: {
+        type: Boolean,
+        default: false,
+      }
     },
     data() {
       return {
         // 默认的空图片
         defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png',
+      }
+    },
+    methods: {
+      radioClickHandler() {
+        this.$emit('radio-change', {
+          goods_id: this.goods.goods_id,
+          goods_state: !this.goods.goods_state
+        })
+      },
+      // NumberBox 组件的 change 事件处理函数
+      numChangeHandler(val) {
+        // 通过 this.$emit() 触发外界通过 @ 绑定的 num-change 事件
+        this.$emit('num-change', {
+          // 商品的 Id
+          goods_id: this.goods.goods_id,
+          // 商品的最新数量
+          goods_count: +val
+        })
       }
     },
     filters: {
@@ -43,12 +72,19 @@
 
 <style lang="scss">
   .goods-item {
+    // 让 goods-item 项占满整个屏幕的宽度
+    width: 750rpx;
+    // 设置盒模型为 border-box
+    box-sizing: border-box;
     display: flex;
     padding: 10px 5px;
     border-bottom: 1px solid #f0f0f0;
 
     .goods-item-left {
       margin-right: 5px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
 
       .goods-pic {
         width: 100px;
@@ -59,11 +95,18 @@
 
     .goods-item-right {
       display: flex;
+      flex: 1;
       flex-direction: column;
       justify-content: space-between;
 
       .goods-name {
         font-size: 13px;
+      }
+
+      .goods-info-box {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
       }
 
       .goods-price {
--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -22,7 +22,7 @@
         </view>
       </view>
       <!-- 运费 -->
-      <view class="yf">快递：免运费</view>
+      <view class="yf">快递：免运费 </view>
       <!-- 商品详情信息 -->
       <rich-text :nodes="goods_info.goods_introduce"></rich-text>
 
@@ -41,7 +41,30 @@
 </template>
 
 <script>
+  import {
+    mapState,
+    mapMutations,
+    mapGetters
+  } from 'vuex'
   export default {
+    computed: {
+      ...mapState('m_cart', ['cart']),
+      ...mapGetters('m_cart', ['total']),
+    },
+    watch: {
+      // 定义 total 侦听器，指向一个配置对象
+      total: {
+        // handler 属性用来定义侦听器的 function 处理函数
+        handler(newVal) {
+          const findResult = this.options.find(x => x.text === '购物车')
+          if (findResult) {
+            findResult.info = newVal
+          }
+        },
+        // immediate 属性用来声明此侦听器，是否在页面初次加载完毕后立即调用
+        immediate: true
+      }
+    },
     data() {
       return {
         // 商品详情对象
@@ -53,7 +76,7 @@
         }, {
           icon: 'cart',
           text: '购物车',
-          info: 2
+          info: 0
         }],
         // 右侧按钮组的配置对象
         buttonGroup: [{
@@ -76,6 +99,8 @@
       this.getGoodsDetail(goods_id)
     },
     methods: {
+      // 把 m_cart 模块中的 addToCart 方法映射到当前页面使用
+      ...mapMutations('m_cart', ['addToCart']),
       // 定义请求商品详情数据的方法
       async getGoodsDetail(goods_id) {
         const {
@@ -85,7 +110,8 @@
         })
         if (res.meta.status !== 200) return uni.$showMsg()
         // 使用字符串的 replace() 方法，为 img 标签添加行内的 style 样式，从而解决图片底部空白间隙的问题
-        res.message.goods_introduce = res.message.goods_introduce.replace(/<img /g, '<img style="display:block;" ')
+        res.message.goods_introduce = res.message.goods_introduce.replace(/<img /g,
+            '<img style="display:block;" ')
           .replace(/webp/g, 'jpg')
         // 为 data 中的数据赋值
         this.goods_info = res.message
@@ -103,6 +129,27 @@
             url: '/pages/cart/cart'
           })
         }
+      },
+      // 右侧按钮的点击事件处理函数
+      buttonClick(e) {
+        // 1. 判断是否点击了 加入购物车 按钮
+        if (e.content.text === '加入购物车') {
+
+          // 2. 组织一个商品的信息对象
+          const goods = {
+            goods_id: this.goods_info.goods_id, // 商品的Id
+            goods_name: this.goods_info.goods_name, // 商品的名称
+            goods_price: this.goods_info.goods_price, // 商品的价格
+            goods_count: 1, // 商品的数量
+            goods_small_logo: this.goods_info.goods_small_logo, // 商品的图片
+            goods_state: true // 商品的勾选状态
+          }
+
+          // 3. 通过 this 调用映射过来的 addToCart 方法，把商品信息对象存储到购物车中
+          this.addToCart(goods)
+
+        }
+
       }
     }
   }
